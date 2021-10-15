f90b21a3657f3a76ac1c3dbc4432f4bc6fe45576
Kaderbv <80204419+Kaderbv@users.noreply.github.com>
2021-10-15 15:09:40 +0530

    footer style updated

--- components/saint/FooterNav.tsx
+++ components/saint/FooterNav.tsx
@@ -7,18 +7,20 @@ function FooterNav() {
             <div className="footerlogo-container">
             <Link href="/">
                     <img  src="/bonbon_footer.svg" alt="Logo" />
+                 
                 </Link>
-                </div>   <div className="footer-container">
-              <h3> <Link   href="/"><a className="active">bonbon</a></Link> is lovingly powered by:</h3>
+                </div>   
+            <div className="footertext-container">
+            <h3><Link   href="/"><a className="active">bonbon</a></Link> is lovingly powered by:</h3>
             </div>
             <div className="footer-container">
          
-            <Link   href="https://metasafari.world/"><a className="active">Metasafari</a></Link>
-            <Link   href="https://metasafari.world/"><a className="active">Wildlife Park</a></Link>
-            <Link   href="https://solana.com/"><a className="active">Solana</a></Link>
-            <Link   href="https://metaplex.com/"><a className="active">Metaplex</a></Link>
-            <Link   href="https://hackmd.io/@levicook/HJcDneEWF"><a className="active">Candy Machine</a></Link>
-            <Link   href="https://phantom.app/"><a className="active">Phantom</a></Link>
+            <Link   href="https://metasafari.world/"><a className="active" target="_blank">Metasafari</a></Link>
+            <Link   href="https://metasafari.world/"><a className="active" target="_blank">Wildlife Park</a></Link>
+            <Link   href="https://solana.com/"><a className="active" target="_blank">Solana</a></Link>
+            <Link   href="https://metaplex.com/"><a className="active" target="_blank">Metaplex</a></Link>
+            <Link   href="https://hackmd.io/@levicook/HJcDneEWF" target="_blank"><a className="active">Candy Machine</a></Link>
+            <Link   href="https://phantom.app/"><a className="active" target="_blank">Phantom</a></Link>
 
             </div>
         </nav>
